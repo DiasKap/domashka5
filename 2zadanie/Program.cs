e91c64c94323633594349626fc4dee5f203e8ec1
DiasKapassov <diasgoose@gmail.com>
2023-04-10 17:43:20 +0600

    3 задание

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] Size_array(int size, int minv, int maxv){
+    int[] array= new int[size];
+    for (int i=0; i<size;i++){
+        array[i]=new Random().Next(minv,maxv);
+    }
+    return array;
+}
+int count(int[] array){
+    int count=0;
+    for(int i=0; i<array.Length; i++){
+        if (i%2!=0){
+            count+=array[i];
+    }}
+    return count;}
+int[] array=Size_array(5,10,100);  
+Console.WriteLine(count(array));
+Console.Write(String.Join(", ", array));  
