265dab59b65edf0a0f6eff98e9311d76e11a5119
DiasKapassov <diasgoose@gmail.com>
2023-04-10 17:43:44 +0600

    сделал 3

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] Size_array(int size, int minv, int maxv){
+    int[] array= new int[size];
+    for (int i=0; i<size;i++){
+        array[i]=new Random().Next(minv,maxv);
+    }
+    return array;
+}
+int difference(int[] array){
+    int min=array[0];
+    int max=array[0];
+    foreach(int el in array){
+    if (el<min){
+        min=el;}
+    else if (el>max){
+        max=el;}}
+    return (max-min);
+}
+int[] array=Size_array(5,10,100);  
+Console.WriteLine(difference(array));
+Console.Write(String.Join(", ", array));
